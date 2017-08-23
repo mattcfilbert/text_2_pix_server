ff03b8c7d426535b28047dbc95d07b1e0506c358
Jacob Greenwald <jeg4t@virginia.edu>
2017-08-23 15:17:36 -0400

    deploying

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -33,6 +33,6 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:text, :meme_id)
+    params.require(:comment).permit(:text)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :memes
   resources :comments
+
+  # match '*path' => 'home#index', via: :get
+
 end
